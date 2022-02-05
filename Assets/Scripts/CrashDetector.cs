7698b12ec2c1bed260ea2884d2d9a74e026fa154
Ian Montgomery <ianarthurmontgomery@gmail.com>
2022-02-05 16:23:08 +0000

    Level restarts on crash/finish

--- Assets/Scripts/CrashDetector.cs
+++ Assets/Scripts/CrashDetector.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CrashDetector : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 2f;
     void OnTriggerEnter2D(Collider2D other)
     {
         if(other.tag == "Ground")
         {
-            Debug.Log("You hit your head");
+            Invoke("ReloadScene", reloadDelay);
         }
+    }
+
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
     }
 }
--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
+    [SerializeField] float reloadDelay = 2f;
+
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
         {
-            Debug.Log("You finished");
+            Invoke("ReloadScene", reloadDelay);
+            // Debug.Log("You finished");
         }
     }
-
+    void ReloadScene()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
